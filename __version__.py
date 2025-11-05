4dcdd13fbc63fdbf73b1224e13e78e27f4b911f1
Ian Heidt <iheidt@hotmail.com>
2025-11-04 22:34:58 -0800

    bump version

--- __version__.py
+++ __version__.py
@@ -1,9 +1,10 @@
 """Version information for NRHOF Matrix Kiosk."""
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 __version_info__ = tuple(int(x) for x in __version__.split("."))
 
 # Version history
+# 0.2.0 - Major refactoring: modular architecture (core/, routing/, scenes/), UI components split
 # 0.1.3 - Mac Mini deployment, custom fonts, renderer abstraction
 # 0.1.2 - Audio visualizations complete
 # 0.1.1 - Voice commands integrated
